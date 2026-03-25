daf4396a9f8b4caecb59b0b4534ceda1bb9e0484
Knife <knife@KnifedeMacBook-Air.local>
2026-03-25 15:50:10 +0800

    Initial commit of python-learn project

---
+++ prac.py
@@ -0,0 +1,37 @@
+# name = "Knife"
+# age = "18"
+# print(f"My name is {name},I am {age} years old.")
+# print("My name is %s ,I am %s years old"%(name,age))
+#
+# a = 10
+# b = 3.14
+# c = "hello"
+# print(type(a),type(b),type(c))
+#
+# a = "hello"
+# b = "world"
+# print(a + " " + b)
+#
+# a,b = input("Enter two numbers separated by a comma").split()
+# a = int(a)
+# b = int(b)
+# print(a+b)
+# print(a*b)
+#
+# a = 20
+# b = 10
+# c = a
+# a = b
+# b = c
+# print(a,b)
+#
+# a = input("姓名：")
+# b = input("年龄：")
+# c = input("身高：")
+# print(f"====个人信息====\n姓名:{a}\n年龄:{b}\n身高：{c}")
+#
+# a = "10"
+# b = 5
+# print(a + str(b))
+# print(int(a) + b)
+
